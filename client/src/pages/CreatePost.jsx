50fd3fc908f4107ca9ed3fffe17b740c3b305bbd
Aa-sheesh <aashish4912345@gmail.com>
2025-03-24 01:51:35 +0530

    update API endpoints to use production URL

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ function CreatePost() {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://image-io-tdc9.onrender.com", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -49,7 +49,7 @@ function CreatePost() {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/imageio", {
+        const response = await fetch("https://image-io-tdc9.onrender.com", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
